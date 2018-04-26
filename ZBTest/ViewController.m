7e9a1eb8177537d34ffbda66129afd09198ca337
qq_33147323 <756835906@qq.com>
2018-04-26 17:47:55 +0800

    0.0.1

--- ZBTest/ViewController.m
+++ ZBTest/ViewController.m
@@ -7,41 +7,21 @@
 //
 
 #import "ViewController.h"
-#import "BinaryTreeNode.h"
-#import "BinaryTree.h"
-@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
-@property (nonatomic,strong)UITableView *myTable;
+
+@interface ViewController ()
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.myTable = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
-    self.myTable.delegate = self;
-    self.myTable.dataSource = self;
-    self.myTable.tableFooterView = [UIView new];
-    [self.view addSubview:self.myTable];
+ 
    
   
 }
 
--(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString *cellid = @"cellid";
-    UITableViewCell*cell = [tableView dequeueReusableCellWithIdentifier:cellid];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellid];
-    }
-    return cell;
-}
 
-- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 20;
-}
-
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
-    cell.textLabel.text = [NSString stringWithFormat:@"%ld",(long)indexPath.row];
-}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
